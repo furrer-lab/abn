660ece9a046a558f167149f784456609fb05a3ff
Matteo Delucchi <mail@matteodelucchi.com>
2025-03-03 13:11:57 +0100

    reintendation

--- R/markov-blanket.R
+++ R/markov-blanket.R
@@ -57,67 +57,67 @@ mb <- function(dag, node, data.dists=NULL) {
     stop("Incorrect node specification 'node' to compute the Markov Blanket")
   }
 
-    ## as of older version.
-    # row parent column children mb.node.final <- list()
-    mb.node.tmp <- list()
-    for (n.element in node) {
+  ## as of older version.
+  # row parent column children mb.node.final <- list()
+  mb.node.tmp <- list()
+  for (n.element in node) {
 
-        ## Parent + Children
-        mb.children <- list()
-        mb.parent <- list()
-        for (i in 1:length(dag[1, ])) {
-            if (dag[i, n.element] != 0) {
-                mb.children[i] <- names(dag[, n.element])[i]
-            }
-            if (dag[n.element, i] != 0) {
-                mb.parent[i] <- names(dag[n.element, ])[i]
-            }
+    ## Parent + Children
+    mb.children <- list()
+    mb.parent <- list()
+    for (i in 1:length(dag[1, ])) {
+      if (dag[i, n.element] != 0) {
+        mb.children[i] <- names(dag[, n.element])[i]
+      }
+      if (dag[n.element, i] != 0) {
+        mb.parent[i] <- names(dag[n.element, ])[i]
+      }
+    }
+    # delete NULL element
+    mb.children <- unlist(mb.children[!sapply(mb.children, is.null)])
+    mb.parent <- unlist(mb.parent[!sapply(mb.parent, is.null)])
+
+    ## Parent of children
+    mb.parent.children <- list()
+    for (node.children in mb.children) {
+      for (i in 1:length(dag[1, ])) {
+        if (dag[node.children, i] != 0) {
+          mb.parent.children[i] <- names(dag[node.children, ])[i]
         }
-        # delete NULL element
-        mb.children <- unlist(mb.children[!sapply(mb.children, is.null)])
-        mb.parent <- unlist(mb.parent[!sapply(mb.parent, is.null)])
+      }
+    }
+    # delete NULL element
+    mb.parent.children <- unlist(mb.parent.children[!sapply(mb.parent.children, is.null)])
 
-        ## Parent of children
-        mb.parent.children <- list()
-        for (node.children in mb.children) {
-            for (i in 1:length(dag[1, ])) {
-                if (dag[node.children, i] != 0) {
-                    mb.parent.children[i] <- names(dag[node.children, ])[i]
-                }
-            }
+    # add all list
+    mb.node <- unlist(list(mb.children, mb.parent, mb.parent.children))
+
+    # unique element
+    mb.node <- unique(mb.node)
+
+    # delete index node
+    mb.node.wo <- NULL
+    if (length(mb.node) != 0) {
+      for (i in 1:length(mb.node)) {
+        if (mb.node[c(i)] == n.element) {
+          mb.node.wo <- mb.node[-c(i)]
         }
-        # delete NULL element
-        mb.parent.children <- unlist(mb.parent.children[!sapply(mb.parent.children, is.null)])
+      }
+    }
+    if (is.null(mb.node.wo)) {
+      mb.node.wo <- mb.node
+    }
 
-        # add all list
-        mb.node <- unlist(list(mb.children, mb.parent, mb.parent.children))
+    ## store out of loop
+    mb.node.tmp <- unlist(list(mb.node.tmp, mb.node.wo))
+    # unique element
+    mb.node.tmp <- unique(mb.node.tmp)
+    # delete NULL element
+    mb.node.tmp <- unlist(mb.node.tmp[!sapply(mb.node.tmp, is.null)])
 
-        # unique element
-        mb.node <- unique(mb.node)
+  }  #EOF loop through node
 
-        # delete index node
-        mb.node.wo <- NULL
-        if (length(mb.node) != 0) {
-            for (i in 1:length(mb.node)) {
-                if (mb.node[c(i)] == n.element) {
-                    mb.node.wo <- mb.node[-c(i)]
-                }
-            }
-        }
-        if (is.null(mb.node.wo)) {
-            mb.node.wo <- mb.node
-        }
-
-        ## store out of loop
-        mb.node.tmp <- unlist(list(mb.node.tmp, mb.node.wo))
-        # unique element
-        mb.node.tmp <- unique(mb.node.tmp)
-        # delete NULL element
-        mb.node.tmp <- unlist(mb.node.tmp[!sapply(mb.node.tmp, is.null)])
-
-    }  #EOF loop through node
-
-    return(mb.node.tmp)
+  return(mb.node.tmp)
 }
 
 
