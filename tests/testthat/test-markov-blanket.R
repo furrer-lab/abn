5162d94542e9f7cf498b7b23e3ef569c96a35c66
Matteo Delucchi <mail@matteodelucchi.com>
2025-03-03 12:06:24 +0100

    added test for reproducing the reportet error.

--- tests/testthat/test-markov-blanket.R
+++ tests/testthat/test-markov-blanket.R
@@ -17,3 +17,16 @@ test_that("Markov blanket is correctly retrieved", {
   expect_equal(b, c("a", "f", "b", "c"))
   expect_equal(c, c("a", "c", "d", "f", "e", "b"))
 })
+
+test_that("Markov blanket is correctly retrieved 2", {
+  dag <- matrix(c(0,1,0,1,1,0,0,1,1,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0),nrow=5,byrow=TRUE)
+  colnames(dag) =rownames(dag) <- c("g1","g2","b1","b2","b3")
+
+  data.dists_wrongOrder <- list("b1" = "binomial", "b2" = "binomial", b3 = "binomial", g1 = "gaussian", g2 = "gaussian")
+  data.dists_correctOrder <- list("g1" = "gaussian", "g2" = "gaussian", "b1" = "binomial", "b2" = "binomial", "b3" = "binomial")
+
+
+  # TODO: Check order of data.dists must be the same as in colnames(dag)
+  expect_equal(abn::mb(dag,node="b1",data.dists_correctOrder), c("g2", "b3", "b2"))
+  expect_error(abn::mb(dag,node="b1",data.dists_wrongOrder))
+})
