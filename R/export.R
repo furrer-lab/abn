8d9b6ec1e97133fee485b8cafcdb773d1360ff73
Matteo Delucchi <mail@matteodelucchi.com>
2025-09-10 13:59:04 +0200

    refactored for being ready for other export formats.

--- R/export.R
+++ R/export.R
@@ -1,30 +1,33 @@
 #' Export abnFit object to structured format
 #'
 #' @param object An object of class abnFit
-#' @param format Output format ("json")
+#' @param format The export format, currently only "json" is supported.
 #' @param include_network Whether to include network structure
 #' @param file Optional file path to save JSON. If NULL, returns JSON string
 #' @param pretty Logical, whether to format JSON nicely
 #' @param ... Additional export options
-#' @return JSON string or file path (invisibly)
-export_abnFit <- function(object, format = "list", include_network = TRUE, file = NULL, pretty = TRUE, ...) {
+#' @return A JSON string or writes to file if 'file' is provided
+export_abnFit <- function(object, format = "json", include_network = TRUE, file = NULL, pretty = TRUE, ...) {
   if (!inherits(object, "abnFit")) {
     stop("Input object must be of class 'abnFit'")
   }
 
-  # # Dispatch based on method
-  # switch(object$method,
-  #        mle = export_mle(object, format, include_network, ...),
-  #        bayes = export_bayes(object, format, include_network, ...),
-  #        stop(paste0("Unsupported method in abnFit object. Method must be 'mle' or 'bayes', but found: ", object$method))
-  # )
+  # Dispatch based on method
+  if (object$method == "mle") {
+    export_list <- export_abnFit_mle(object, format, include_network, ...)
+  } else if (object$method == "bayes") {
+    export_list <- export_abnFit_bayes(object, format, include_network, ...)
+  } else {
+    stop("Unsupported method in abnFit object. Supported methods are 'mle' and 'bayes'.")
+  }
 
-  # Start with minimal structure to pass first test
-  json_list <- list(
-    graph = list(),
-    nodes = list(),
-    arcs = list()
-  )
+  # Convert to desired format
+  if (format == "json") {
+    return(export_to_json(export_list, format, file, pretty))
+  } else {
+    stop("Currently, only 'json' format is supported.")
+  }
+}
 
   json_str <- jsonlite::toJSON(json_list, pretty = pretty, auto_unbox = TRUE)
 
